5de16f021a3211a18156ff6046d959c689f05ec6
ronak <ronakpareek96@gmail.com>
2025-11-27 12:01:17 +0530

    demo-doc-v1

---
+++ src/main/java/com/demodoc/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.demodoc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+		@GetMapping("/message")
+		public String getMessage() {
+			return "Hello-world";
+		}
+}
---
+++ src/test/java/com/demodoc/DemoDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demodoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
